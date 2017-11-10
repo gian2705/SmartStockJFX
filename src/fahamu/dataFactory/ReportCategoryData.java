e6e2dca118a0cd1c9ae03e41c8a0be708fe0824a
joshua <mama27j@gmail.com>
2017-11-10 09:37:10 +0300

    change how to get profit

--- src/fahamu/dataFactory/ReportCategoryData.java
+++ src/fahamu/dataFactory/ReportCategoryData.java
@@ -10,6 +10,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import static fahamu.UserInteface.LogInStage.*;
 import static fahamu.UserInteface.ReportsCategoryUI.purchaseSeries;
@@ -108,6 +110,14 @@ public class ReportCategoryData {
 
         Connection connection = null;
         ObservableList<ReportsCategoryUI.GrossProfitTableViewData> data = FXCollections.observableArrayList();
+        /*
+        the hash map used to store product as key
+        and its quantity sold as value. And the List store products as key
+        to retrieve the value next
+         */
+        HashMap<String, Integer> productQuantityMap = new HashMap<>();
+        ArrayList<String> products = new ArrayList<>();
+
         try {
             try {
 
@@ -123,73 +133,86 @@ public class ReportCategoryData {
              */
             categoryList.clear();
             StockCategoryData.getAllCategories();
+
+            /*
+            get all product sold in a given range'
+            statement and resultSet variable used as a global-local variable
+             */
             Statement statement;
             ResultSet resultSet;
 
+            String selectAllSoldProducts = "SELECT product,quantity " +
+                    "FROM salesdata.cashSale " +
+                    "WHERE date>=\'" + fromDate + "\' AND date<=\'" + toDate + "\'";
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(selectAllSoldProducts);
+            while (resultSet.next()) {
+                String product = resultSet.getString("product");
+                int quantity = resultSet.getInt("quantity");
+
+                products.add(product);
+                productQuantityMap.put(product, quantity);
+            }
+
+            /*
+            now we iterate the sum of profit of all sold products
+             */
             for (String category :
                     categoryList) {
-                String selectSumOfSalesQuery = "SELECT sum(amount) as sum " +
-                        "FROM salesdata.cashSale where category=\'" + category + "\' " +
-                        "AND date>=\'" + fromDate + "\' AND date<=\'" + toDate + "\'";
 
-                /*
-                because in database we have two source of purchase,
-                so cash and credit purchase found separate and their sum
-                calculated
-                 */
-                String selectSumOfCashPurchaseQuery = "SELECT sum(amount) as sum " +
-                        "FROM purchasedata.cashPurchase where category=\'" + category + "\' " +
-                        "AND date>=\'" + fromDate + "\' AND date<=\'" + toDate + "\'";
+                for (String product :
+                        products) {
 
-                String selectSumOfCreditPurchaseQuery = "SELECT sum(amount) as sum " +
-                        "FROM purchasedata.creditPurchase where category=\'" + category + "\' " +
-                        "AND date>=\'" + fromDate + "\' AND date<=\'" + toDate + "\'";
+                    String selectProfitOfProductFromStock = "SELECT profit  " +
+                            "FROM stockdata.retailStock " +
+                            "WHERE product=\'" + product + "\' AND category=\'" + category + "\'";
 
                 /*
                 This variables used to populate gross profit table
                 sum is used to find total of credit and cash purchase,
                 initialized to zero every time loop repeat
                 */
-                float saleSum = 0;
-                float sumPurchase = 0;
-                int grossProfitSum;
+                    float saleSum = 0;
+                    float sumPurchase = 0;
+                    int grossProfitSum;
 
                 /*
-                find sum of sales for given category
+                find sum of profit for given product and category
                  */
-                statement = connection.createStatement();
-                resultSet = statement.executeQuery(selectSumOfSalesQuery);
-                while (resultSet.next()) {
-                    saleSum = resultSet.getFloat("sum");
-                }
+                    statement = connection.createStatement();
+                    //resultSet = statement.executeQuery(selectSumOfSalesQuery);
+                    while (resultSet.next()) {
+                        saleSum = resultSet.getFloat("sum");
+                    }
 
                 /*
                 to find sum of all kind of purchase
                  */
-                statement = connection.createStatement();
-                resultSet = statement.executeQuery(selectSumOfCashPurchaseQuery);
-                while (resultSet.next()) {
-                    sumPurchase = resultSet.getFloat("sum");
-                }
-                resultSet = statement.executeQuery(selectSumOfCreditPurchaseQuery);
-                while (resultSet.next()) {
-                    sumPurchase = sumPurchase + resultSet.getFloat("sum");
-                }
+                    statement = connection.createStatement();
+                    //resultSet = statement.executeQuery(selectSumOfCashPurchaseQuery);
+                    while (resultSet.next()) {
+                        sumPurchase = resultSet.getFloat("sum");
+                    }
+                    //resultSet = statement.executeQuery(selectSumOfCreditPurchaseQuery);
+                    while (resultSet.next()) {
+                        sumPurchase = sumPurchase + resultSet.getFloat("sum");
+                    }
 
-                grossProfitSum = (int) (saleSum - sumPurchase);
+                    grossProfitSum = (int) (saleSum - sumPurchase);
 
                 /*
                 populate the gross profit table with data
                  */
 
-                data.add(new ReportsCategoryUI.GrossProfitTableViewData(
-                        category,
-                        saleSum,
-                        sumPurchase,
-                        grossProfitSum
-                ));
+                    data.add(new ReportsCategoryUI.GrossProfitTableViewData(
+                            category,
+                            saleSum,
+                            sumPurchase,
+                            grossProfitSum
+                    ));
 
 
+                }
             }
 
         } catch (SQLException e) {
