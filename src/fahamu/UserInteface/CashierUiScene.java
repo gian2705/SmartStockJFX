4c51d0b81bbfbdf8ff8382794e231bbb794ba1f1
olive <mama27j@gmail.com>
2018-02-10 13:35:54 +0300

    update cashier scene

--- src/fahamu/UserInteface/CashierUiScene.java
+++ src/fahamu/UserInteface/CashierUiScene.java
@@ -1,5 +1,17 @@
 package fahamu.UserInteface;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
 public class CashierUiScene {
+
+    @FXML
+    public JFXButton addToCartJFXButton;
+
+    @FXML
+    public void initialize(){
+        addToCartJFXButton.setGraphic(new ImageView(getClass().getResource("data/cart.png").toExternalForm()));
+    }
 
 }
